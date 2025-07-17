7a913cfb0c8d7781d55f642e73705c6ff77f2fbd
nari <yasunarj@gmail.com>
2025-07-17 16:50:26 +0900

    fixed: getWeathre.ts

--- app/lib/line/getReplyMessage.ts
+++ app/lib/line/getReplyMessage.ts
@@ -42,7 +42,7 @@ export const getReplyMessage = async (userText: string) => {
   if (text.includes("私の住所")) return myAddress;
   if (text.includes("私のパスワード")) return myPassword;
   if (text.includes("今日の天気")) {
-    const message = await getWeather();
+    const message = await getWeather("Tokyo");
     return `お問い合わせありがとうございます。 ${message}`;
   }
   // if (text.includes("")) return "";
